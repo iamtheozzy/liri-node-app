8ab31e82da7f23297f085a0c5b54ad175e82a870
Ozzy Cardenas <ocardenas789@me.com>
2018-02-09 23:02:51 -0600

    added some baic logic and program confits

--- liri.js
+++ liri.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+
+
+// requires files where they keys are stored
+var keys = requre("./keys.js");
+var Twitter = require('twitter');
+
+// storing variables for node selectors
+var node = process.argv;
+var commands = node[2];
+
+if (commands === "my-tweets"){
+
+
+}
