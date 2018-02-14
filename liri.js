4abe20e99cb436ac175601af2941f12e2ef27807
Ozzy Cardenas <ocardenas789@me.com>
2018-02-14 13:05:40 -0600

    Finished the OMBD functionality of the app

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require("request");
 
 // storing variables for node selectors
 var node = process.argv;
@@ -45,7 +46,6 @@ if (commands === "my-tweets") {
 
 // Controls spotify request from user
 if (commands === "spotify-this-song") {
-
 
 
   var output = "";
@@ -92,4 +92,40 @@ if (commands === "spotify-this-song") {
       };
 
     });
-}
+};
+
+if (commands === "movie-this") {
+
+  // stores multiple user inputs
+  var output = "";
+  for (var i = 3; i < process.argv.length; i++) {
+    output += process.argv[i] + "+";
+  };
+
+  output = output.substring(0, output.length - 1);
+
+  var movieTitle = output ? output : "Mr.Nobody";
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+  request(queryUrl, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      // console.log(JSON.parse(body));
+      console.log("Movie Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMBD Rating : " + JSON.parse(body).Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating : " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country : " + JSON.parse(body).Country);
+      console.log("Language : " + JSON.parse(body).Language);
+      console.log("Plot : " + JSON.parse(body).Plot);
+      console.log("Actors : " + JSON.parse(body).Actors);
+    }
+  });
+
+
+};
